a031e1d33cd7b1b18fe7e8751574c4b033117dce
Heather Empson <hrempson@gmail.com>
2020-09-10 18:38:16 -0700

    deploying on heroku

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,13 @@ var connection = mysql.createConnection( {
     database: "burgers_DB"
 });
 
+JAWSDB_URL = 'mysql://ihip14gxncu76hnw:cvrbdsj8s8lt5nsq@w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gth6buwxby9brkex'
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
+        PORT: 3306,
         host:'localhost',
         user: 'root',
         password: 'password',
